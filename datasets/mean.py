e5d0824c9ab10f531eec771966dffe290ff3fd74
tboulet <timothe.boulet0@gmail.com>
2024-03-02 18:39:43 +0100

    add reg dataset

--- datasets/mean.py
+++ datasets/mean.py
@@ -11,37 +11,18 @@ class MeanDataset(BaseDataset):
         # Parameters of the dataset
         self.p = config["dim"]
         self.n = self.p  # The number of samples is equal to the dimension of the data for the mean dataset
-        self.non_zero_student_t_scale = config["non_zero_student_t_scale"]
-        self.non_zero_student_t_degree_freedom = config[
-            "non_zero_student_t_degree_freedom"
-        ]
         self.sigma = config["sigma"]
 
         # Generate the beta vector
-        if config["non_zero_first_coefficients"] is not None:
-            non_zero_first_coefficients = config["non_zero_first_coefficients"]
-            assert (
-                len(non_zero_first_coefficients) <= self.p
-            ), "The number of non-zero coefficients should be less than the dimension of the data."
-            self.beta = np.zeros(self.p)
-            self.beta[: len(non_zero_first_coefficients)] = non_zero_first_coefficients
-
-        else:
-            if config["non_zero_proportion"] is not None:
-                non_zero_proportion = config["non_zero_proportion"]
-            else:
-                non_zero_proportion = np.random.uniform(
-                    config["non_zero_proportion_min"], config["non_zero_proportion_max"]
-                )
-            self.beta = np.zeros(shape=(self.p,))
-            for j in range(self.p):
-                if np.random.rand() < non_zero_proportion:
-                    self.beta[j] = (
-                        np.random.standard_t(
-                            self.non_zero_student_t_degree_freedom,
-                        )
-                        * self.non_zero_student_t_scale
-                    )
+        self.beta = self.generate_beta(
+            p=self.p,
+            non_zero_first_coefficients=config["non_zero_first_coefficients"],
+            non_zero_proportion=config["non_zero_proportion"],
+            non_zero_proportion_min=config["non_zero_proportion_min"],
+            non_zero_proportion_max=config["non_zero_proportion_max"],
+            non_zero_student_t_degree_freedom=config["non_zero_student_t_degree_freedom"],
+            non_zero_student_t_scale=config["non_zero_student_t_scale"],
+        )
                     
         # Generate the data
         self.X_matrix = np.eye(self.p)
