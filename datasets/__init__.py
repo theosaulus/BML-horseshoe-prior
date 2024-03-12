86af6b278fa6e21be6a7a949c53a9cdea22f4ab9
ramlaoui <ramlaoui@outlook.com>
2024-03-12 00:04:18 +0000

    classification in init

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,9 +2,11 @@ from typing import Dict, Type
 from datasets.base_dataset import BaseDataset
 from datasets.mean import MeanDataset
 from datasets.regression import RegressionDataset
+from datasets.classification import ClassificationDataset
 
 
 dataset_name_to_DatasetClass: Dict[str, Type[BaseDataset]] = {
     "Mean Dataset": MeanDataset,
     "Regression Dataset": RegressionDataset,
+    "Classification Dataset": ClassificationDataset
 }
